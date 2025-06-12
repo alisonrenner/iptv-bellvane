cd78fa1cb580a054a01f84913ede9fa056d5fbc2
alisonrenner <alisonrenner86@gmail.com>
2025-06-12 03:27:03 -0300

    Add files via upload

--- gerar_m3u.py
+++ gerar_m3u.py
@@ -1,5 +1,13 @@
 import json
+import requests
 from datetime import datetime
+
+def link_ativo(url):
+    try:
+        resposta = requests.get(url, timeout=5, stream=True)
+        return resposta.status_code == 200
+    except:
+        return False
 
 with open("canais.json", "r", encoding="utf-8") as f:
     canais = json.load(f)
@@ -8,5 +16,7 @@ with open("lista.m3u", "w", encoding="utf-8") as m3u:
     m3u.write(f"#EXTM3U\n# Atualizado em: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n")
     for nome, urls in canais.items():
         if isinstance(urls, list) and urls:
-            url = urls[0]  # usa o primeiro link válido
-            m3u.write(f'#EXTINF:-1,{nome}\n{url}\n')
+            for url in urls:
+                if link_ativo(url):
+                    m3u.write(f'#EXTINF:-1,{nome}\n{url}\n')
+                    break

cf28551e8d06982f2389404ca063ae406709c224
alisonrenner <alisonrenner86@gmail.com>
2025-06-12 02:54:00 -0300

    Update gerar_m3u.py

--- gerar_m3u.py
+++ gerar_m3u.py
@@ -1,21 +1,21 @@
 import json
 
 with open("canais.json", "r", encoding="utf-8") as f:
-    dados = json.load(f)
+    canais = json.load(f)
 
 with open("lista.m3u", "w", encoding="utf-8") as m3u:
     m3u.write("#EXTM3U\n")
-    for canal in dados["canais"]:
+    for canal in canais:
         if isinstance(canal, dict):
             url = None
-            if isinstance(canal["url"], list):
+            if isinstance(canal.get("url"), list):
                 for u in canal["url"]:
-                    if u:  # usa o primeiro válido
+                    if u:
                         url = u
                         break
-            else:
+            elif isinstance(canal.get("url"), str):
                 url = canal["url"]
 
             if url:
-                m3u.write(f'#EXTINF:-1 tvg-id="{canal["id"]}" group-title="{canal["grupo"]}",{canal["nome"]}\n')
+                m3u.write(f'#EXTINF:-1 tvg-id="{canal.get("id", "")}" group-title="{canal.get("grupo", "")}",{canal.get("nome", "")}\n')
                 m3u.write(f"{url}\n")

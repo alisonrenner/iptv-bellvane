f5a010d07dae83160c69a21cfd55f991a74c51df
alisonrenner <alisonrenner86@gmail.com>
2025-06-12 02:50:00 -0300

    Update gerar_m3u.py

--- gerar_m3u.py
+++ gerar_m3u.py
@@ -1,13 +1,21 @@
 import json
-from datetime import datetime
 
-with open('canais.json', 'r', encoding='utf-8') as f:
-    canais = json.load(f)
+with open("canais.json", "r", encoding="utf-8") as f:
+    dados = json.load(f)
 
-with open('lista.m3u', 'w', encoding='utf-8') as m3u:
-    m3u.write(f"# Arquivo atualizado em: {datetime.now().isoformat()}\n\n")
-    for canal in canais:
-        m3u.write(f"#EXTINF:-1 tvg-id=\"{canal['id']}\" group-title=\"{canal['grupo']}\",{canal['nome']}\n")
-        for link in canal['links']:
-            m3u.write(f"{link}\n")
-        m3u.write("\n")
+with open("lista.m3u", "w", encoding="utf-8") as m3u:
+    m3u.write("#EXTM3U\n")
+    for canal in dados["canais"]:
+        if isinstance(canal, dict):
+            url = None
+            if isinstance(canal["url"], list):
+                for u in canal["url"]:
+                    if u:  # usa o primeiro válido
+                        url = u
+                        break
+            else:
+                url = canal["url"]
+
+            if url:
+                m3u.write(f'#EXTINF:-1 tvg-id="{canal["id"]}" group-title="{canal["grupo"]}",{canal["nome"]}\n')
+                m3u.write(f"{url}\n")
